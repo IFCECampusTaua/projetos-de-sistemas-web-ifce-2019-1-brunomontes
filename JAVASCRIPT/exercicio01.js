ce144f86efae4cd6d537604100faa2a3db24c7a0
bruno montes <brunorsilva75@gmail.com>
2019-04-12 19:11:40 -0300

    Exercise01 JS: including comments

--- JAVASCRIPT/exercicio01.js
+++ JAVASCRIPT/exercicio01.js
@@ -1,5 +1,6 @@
 var botao = document.getElementById("btn");
 
+/*Função criada em um arquivo externo que mostra uma mensagem.*/
 botao.addEventListener('click', function() {
 	alert("Olá Mundo")
 })
